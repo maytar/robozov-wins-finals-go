06f511f5dffa5525c081b2e1afc7c9a923d8c9f3
git-maytarb.gmail.com <maytarb@gmail.com>
2015-04-26 19:29:15 +0300

    Fixing network problems, assuming UDP

--- ControlStation/Transmitter.py
+++ ControlStation/Transmitter.py
@@ -1,9 +1,10 @@
 import socket
 import struct
 
-SERVER_IP = "1.1.1.1"
-SERVER_PORT = 4444
-PROTO_FAMILY = socket.SOCK_STREAM # SOCK_STREAM or SOCK_DGRAM
+SERVER_IP = "10.2.160.1"
+SERVER_PORT = 1234
+ADDRESS = (SERVER_IP, SERVER_PORT)
+PROTO_FAMILY = socket.SOCK_DGRAM # SOCK_STREAM or SOCK_DGRAM
 
 pack = lambda x: chr(x)
 unpack = lambda x: ord(x)
@@ -11,7 +12,7 @@ unpack = lambda x: ord(x)
 class Transmitter:
 	def __init__(self):
 		self.s = socket.socket(socket.AF_INET, PROTO_FAMILY)
-		self.s.connect((SERVER_IP, SERVER_PORT))
+		# self.s.connect((SERVER_IP, SERVER_PORT))
 
 	def send(self, motors):
 		buf = ""
@@ -19,4 +20,6 @@ class Transmitter:
 		buf += pack(motors[1])
 		buf += pack(motors[2])
 
-		self.s.send(buf)
+		print buf
+
+		self.s.sendto(buf,ADDRESS)
